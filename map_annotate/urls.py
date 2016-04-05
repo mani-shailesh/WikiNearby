128c5152d201a8ea0330f229bb22eb30d8ad4cd1
Tushar Agarwal <tushar.agarwal@iitrpr.ac.in>
2016-04-05 23:42:55 +0530

    Initialize views.py, static files and templates

--- map_annotate/urls.py
+++ map_annotate/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from map_annotate_app import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^.*$', views.index, name='index'),
 ]
