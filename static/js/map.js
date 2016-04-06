12bb83ed0a48e564dc0123b0f5990731068f96b2
Shailumani <shailesh.pandey@iitrpr.ac.in>
2016-04-06 17:52:01 +0530

    Resolve merge conflicts

--- static/js/map.js
+++ static/js/map.js
@@ -1,61 +1,16 @@
 // This prototype displays a marker at the center of IIT Ropar.
 // When the user clicks the marker, an info window opens.
 var IITRopar = {lat: 30.9755, lng: 76.5395};
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    return new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: IITRopar,
         mapTypeControl: false
     });
-
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h3 id="firstHeading" class="firstHeading">IIT Ropar</h3>' +
-        '<div id="bodyContent">' +
-        '<p>The Indian Institute of Technology Ropar (IIT Ropar) or IIT-RPR,' +
-        ' is an engineering and technology higher education institute located in Rupnagar, Punjab, India.' +
-        ' It is one of the eight newer Indian Institutes of Technology (IITs) established by the Ministry ' +
-        'of Human Resource Development MHRD, Government of India under The Institutes of Technology (Amendment) Act, ' +
-        '2011. The Act was passed in the Lok Sabha on 24 March 2011 and by the Rajya Sabha on 30 April 2012.' +
-        '</p>' +
-        '<p>Attribution: IIT Ropar, <a target="_blank" href="https://en.wikipedia.org/wiki/Indian_Institute_of_Technology_Ropar">' +
-        'https://en.wikipedia.org/wiki/Indian_Institute_of_Technology_Ropar</a> ' +
-        '</p>' +
-        '</div>' +
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    var marker = new google.maps.Marker({
-        position: IITRopar,
-        map: map,
-        title: 'IIT Ropar'
-    });
-    marker.addListener('click', function () {
-        infowindow.open(map, marker);
-    });
-
-    return map;
-
-    // This example adds a search box to a map, using the Google Place Autocomplete
-    // feature. People can enter geographical searches. The search box will return a
-    // pick list containing a mix of places and predicted search terms.
-
-    // This example requires the Places library. Include the libraries=places
-    // parameter when you first load the API. For example:
-    // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 }
 
 function initAutocomplete() {
-    // var map = new google.maps.Map(document.getElementById('map'), {
-    //     center: IITRopar,
-    //     zoom: 12,
-    //     mapTypeId: google.maps.MapTypeId.ROADMAP
-    // });
 
     var map = initMap();
 
