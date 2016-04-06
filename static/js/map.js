9630af21709e9268abd19ef0e4a73bcc205d424b
Shailumani <shailesh.pandey@iitrpr.ac.in>
2016-04-06 20:30:11 +0530

    Resolve merge conflicts, remove Street-View button

--- static/js/map.js
+++ static/js/map.js
@@ -8,7 +8,8 @@ function initMap() {
     var newMap = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: {lat: -34.397, lng: 150.644},
-        mapTypeControl: false
+        mapTypeControl: false,
+        streetViewControl: false
     });
 
     return newMap;
