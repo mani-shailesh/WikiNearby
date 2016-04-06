a89ccd2b960c62e0f81ade3acf9c773ed9175ba2
Tushar Agarwal <tushar.agarwal@iitrpr.ac.in>
2016-04-06 19:03:28 +0530

    Add filter settings and "My Location" button

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tagarwal on 06/04/16.
+ */
+
+// Filter modal configuration
+
+$('#filterModalTrigger').leanModal({
+        dismissible: true,
+        opacity: .5,
+        in_duration: 300,
+        out_duration: 200,
+        // ready: function() { alert('Ready'); },
+        // complete: function() { alert('Closed'); }
+    }
+);
