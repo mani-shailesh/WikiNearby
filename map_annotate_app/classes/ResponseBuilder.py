c1125bc4bb3f0ad3a47c32ba9e09dbd6c60e21cb
Shailumani <shailesh.pandey@iitrpr.ac.in>
2016-04-11 22:54:03 +0530

    Add stubs in `ResponseBuilder.py`

---
+++ map_annotate_app/classes/ResponseBuilder.py
@@ -0,0 +1,39 @@
+class ResponseBuilder:
+    """
+    Class to build the JSON response to be sent to client.
+    """
+
+    def __init__(self, request):
+        """
+        Initializes the appropriate class variables and filters.
+        :param request: Request sent by the client
+        """
+        pass
+
+    def get_crimes(self):
+        """
+        Helper method to get a list of `Crime` objects which satisfy the filter.
+        :return: List of ``
+        """
+        pass
+
+    def get_wiki_info(self):
+        """
+        Helper method to get a list of `WikiInfo` objects which satisfy the filter.
+        :return:
+        """
+        pass
+
+    def get_legislators(self):
+        """
+        Helper method to get a list of `Legislator` objects which satisfy the filter.
+        :return:
+        """
+        pass
+
+    def get_response(self):
+        """
+        Return the appropriate JSON response which satisfy filters.
+        :return:
+        """
+        pass
--- map_annotate_app/views.py
+++ map_annotate_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+import map_annotate_app.classes.ResponseBuilder as ResponseBuilder
+
 
 def index(request):
     return render(request, 'index.html')
@@ -11,3 +13,5 @@ def get_response(request):
     :param request: Parameters passed in by the client.
     :return:
     """
+    response_builder = ResponseBuilder.ResponseBuilder(request)
+    return response_builder.get_response()
