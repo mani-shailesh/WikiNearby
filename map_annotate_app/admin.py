62a4e0f449a93c38a83b8020b7aa168b660efce8
Shailumani <shailesh.pandey@iitrpr.ac.in>
2016-04-16 02:05:56 +0530

    Register models to admin

--- map_annotate_app/admin.py
+++ map_annotate_app/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Crime
+from .models import CrimeType
+from .models import Location
+from .models import Sansad
+
+admin.site.register(Location)
+admin.site.register(Crime)
+admin.site.register(CrimeType)
+admin.site.register(Sansad)
