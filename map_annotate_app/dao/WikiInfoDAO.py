ff7c11a47bb6e189c6a7e5566664fc2e42546d52
Shailumani <shailesh.pandey@iitrpr.ac.in>
2016-04-16 13:59:09 +0530

    Update WikiDAO to fetch real data without info

--- map_annotate_app/dao/WikiInfoDAO.py
+++ map_annotate_app/dao/WikiInfoDAO.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 from map_annotate_app.dto import WikiInfoDTO
 from map_annotate_app.extras import Location
 
@@ -13,53 +16,32 @@ class WikiInfoDAO:
         :param wiki_filter: `WikiInfoFilter` object to filter out the wiki articles
         :return: List of `WikiInfoDTO` objects which satisfy the `wiki_filter`
         """
-        # TODO
-        # https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gslimit=500&gscoord=28.6139|77.2090&format=json
-        # https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=India
 
         return_list = []
 
-        wiki_dto = WikiInfoDTO.WikiInfoDTO()
-        wiki_dto.location = Location.Location(30.9755, 76.5395)
-        wiki_dto.title = "Indian Institue of Technology Ropar"
-        wiki_dto.link = "https://en.wikipedia.org/?curid=19859118"
-        wiki_dto.info = "This is some sample info...."
-        return_list.append(wiki_dto)
+        url = "https://en.wikipedia.org/w/api.php?" + \
+              "action=query" + \
+              "&list=geosearch" + \
+              "&gsradius=" + str(wiki_filter.radius) + \
+              "&gslimit=500" + \
+              "&gscoord=" + str(wiki_filter.centre.lat) + "|" + str(wiki_filter.centre.lng) + \
+              "&format=json"
 
-        wiki_dto = WikiInfoDTO.WikiInfoDTO()
-        wiki_dto.location = Location.Location(30.9664, 76.5331)
-        wiki_dto.title = "Rupnagar"
-        wiki_dto.link = "https://en.wikipedia.org/?curid=3677968"
-        wiki_dto.info = "This is some sample info...."
-        return_list.append(wiki_dto)
+        response = requests.get(url)
+        if response.ok:
+            json_dict = json.loads(response.content)
+            article_dict_array = json_dict['query']['geosearch']
+            for article_dict in article_dict_array:
+                wiki_dto = WikiInfoDTO.WikiInfoDTO()
+                wiki_dto.location = Location.Location(article_dict['lat'], article_dict['lon'])
+                wiki_dto.title = article_dict['title']
+                wiki_dto.link = "https://en.wikipedia.org/?curid=" + str(article_dict['pageid'])
+                # TODO: Fetch info of page
+                wiki_dto.info = "This is some sample info...."
+                return_list.append(wiki_dto)
+        else:
+            pass
 
-        wiki_dto = WikiInfoDTO.WikiInfoDTO()
-        wiki_dto.location = Location.Location(30.96639, 76.53306)
-        wiki_dto.title = "Rayat Institute of Engineering & Information Technology"
-        wiki_dto.link = "https://en.wikipedia.org/?curid=2211767"
-        wiki_dto.info = "This is some sample info...."
-        return_list.append(wiki_dto)
+        # https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=India
 
-        wiki_dto = WikiInfoDTO.WikiInfoDTO()
-        wiki_dto.location = Location.Location(30.97, 76.51)
-        wiki_dto.title = "Rupnagar district"
-        wiki_dto.link = "https://en.wikipedia.org/?curid=4444855"
-        wiki_dto.info = "This is some sample info...."
-        return_list.append(wiki_dto)
-
-        wiki_dto = WikiInfoDTO.WikiInfoDTO()
-        wiki_dto.location = Location.Location(31.02, 76.5)
-        wiki_dto.title = "Ropar Wetland"
-        wiki_dto.link = "https://en.wikipedia.org/?curid=20093038"
-        wiki_dto.info = "This is some sample info...."
-        return_list.append(wiki_dto)
-
-        wiki_dto = WikiInfoDTO.WikiInfoDTO()
-        wiki_dto.location = Location.Location(31.0392, 76.576)
-        wiki_dto.title = "Nehon"
-        wiki_dto.link = "https://en.wikipedia.org/?curid=5804255"
-        wiki_dto.info = "This is some sample info...."
-        return_list.append(wiki_dto)
-
-        # return_list = random.sample(return_list, 4)
         return return_list
