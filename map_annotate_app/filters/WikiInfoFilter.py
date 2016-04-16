ef64d0f68bf96821f2cc1bc363cfd17cc5b08d1f
Shailumani <shailesh.pandey@iitrpr.ac.in>
2016-04-16 12:50:21 +0530

    Complete WikiInfoFilter, update ResponseBuilder

--- map_annotate_app/filters/WikiInfoFilter.py
+++ map_annotate_app/filters/WikiInfoFilter.py
@@ -1,3 +1,35 @@
+from math import radians, cos, sin, asin, sqrt
+
+from map_annotate_app.extras import Location
+
+
 class WikiInfoFilter:
-    def __init__(self):
-        pass
+    """
+    Class to represent a Filter for fetching WikiInfo data.
+    """
+
+    def __init__(self, north_east, south_west):
+        centre_lat = (north_east.lat + south_west.lat) / 2
+        centre_lng = (north_east.lng + south_west.lng) / 2  # TODO: May cause problems at intersection of +180, -180
+        self.centre = Location.Location(centre_lat, centre_lng)
+        self.radius = haversine(self.centre.lng, self.centre.lat,
+                                north_east.lng, north_east.lat)
+        if self.radius > 10000:
+            self.radius = 10000
+
+
+# Code copied from online resources
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance(in meters) between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    km = 6367 * c
+    return km * 1000
