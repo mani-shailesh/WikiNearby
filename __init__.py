3b341bca435f9be00599d2cfd2f30ac619719d38
Tushar Agarwal <tushar.agarwal@iitrpr.ac.in>
2016-04-26 13:30:48 +0530

    Remove bug inducing __init__.py file in root folder

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The "Map Annotate" project overlays a Google Map with custom geotagged data. Presently,
-crime data (from U{http://www.zipnet.in}) and geotagged Wikipedia articles are supported.
-For more details, see the documentation for C{map_annotate_app}.
-"""
-
-# __all__ = ['map_annotate', 'map_annotate_app']
