261a7fad418b8c27c59ecf4c21a6d2bec5f5519c
Shailesh Mani Pandey <manipandey.shailesh@gmail.com>
2023-04-17 21:59:15 -0700

    Update to use latest version of all dependencies

--- map_annotate/urls.py
+++ map_annotate/urls.py
@@ -1,13 +1,13 @@
 """
 C{map_annotate} URL Configuration
 """
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 
 from map_annotate_app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls, name='admin'),
-    url(r'^api/', views.get_response),
-    url(r'^.*$', views.index, name='index'),
+    re_path(r'^admin/', admin.site.urls, name='admin'),
+    re_path(r'^api/', views.get_response),
+    re_path(r'^.*$', views.index, name='index'),
 ]
